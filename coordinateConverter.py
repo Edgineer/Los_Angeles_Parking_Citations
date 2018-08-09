22ea3759791562d79306d02ceb4198abb9bdc6ba
Edgineer <elopez42@g.ucla.edu>
2018-08-09 15:23:18 -0700

    Added command line arguement for user to input filename of CSV

--- coordinateConverter.py
+++ coordinateConverter.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import requests
 import json
+import argparse
+
+#Command line argument. filename of CSV with data
+parser = argparse.ArgumentParser(description="Convert US Feet Coordinates to WGS84")
+parser.add_argument('filename',type=str,help="filename of CSV file with LA Parking Citation Data to be converted")
+arg = parser.parse_args()
 
 #Read CSV file containing US Feet Coordinates
-data = pd.read_csv('Parking_Citations_sample.csv')
+data = pd.read_csv(arg.filename)
 data.head()
 lat = data['Latitude'].values
 lon = data['Longitude'].values
 
-#File to extract cordinates and format for API request
+#Create file to extract cordinates and put in proper format for API
 filo = open('formattedCoords.txt', 'a+')
 for la,lo in zip(lat,lon):
 	filo.write(str(la)+','+str(lo)+';')
@@ -16,7 +22,7 @@ filo.close()
 
 #API URL
 http = str('http://epsg.io/trans?data=')
-#Codes for conversion from US Feet Coordinates to World Geodetic System 1984
+#Codes for conversion from US Feet Coordinates(102645) to World Geodetic System 1984(4326)
 attr = str('&s_srs=102645&t_srs=4326')
 with open("formattedCoords.txt", "r") as file:
 	coords = file.read()
@@ -24,22 +30,19 @@ file.close()
 
 #Making HTTP Request to the API
 url = http + coords + attr
+#Remove extra semicolon at the end of the coords
 url = url.replace(';&s_srs', '&s_srs')
 page = requests.get(url)
 content = page.content
+print url
+print "Response Code: " + str(page.status_code)
 
-print "Response Code:"
-print page.status_code
-
-print "=================================================Response Body==========================================="
 resData = json.loads(content)
 lats = []
 lons = []
 for d in resData:
 	lats.append(d.get("y"))
 	lons.append(d.get("x"))
-print lats
-print lons
 
 
 dic = {'lat':lats,'lon':lons}
