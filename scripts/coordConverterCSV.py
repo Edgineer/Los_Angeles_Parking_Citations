bb5ac0e8aea970ee1eccff53844b8e629894ac33
Edgineer <elopez42@g.ucla.edu>
2018-09-05 09:49:05 -0700

    edit coord converter for largescale conversion

--- scripts/coordConverterCSV.py
+++ scripts/coordConverterCSV.py
@@ -15,42 +15,52 @@ arg = parser.parse_args()
 #Read CSV file containing US Feet Coordinates
 data = pd.read_csv(arg.filename)
 data.head()
+#Creates numpy.ndarry type
 lat = data['Latitude'].values
 lon = data['Longitude'].values
 
-#Create file to store cordinates in proper format for REST API HTTP Request
-filo = open('formattedCoords.txt', 'a+')
-for la,lo in zip(lat,lon):
-	filo.write(str(la)+','+str(lo)+';')
-filo.close()
+it = 0
+convertedLats = list()
+convertedLons = list()
 
-#API URL
-http = str('http://epsg.io/trans?data=')
-#Codes for conversion from US Feet Coordinates(102645) to World Geodetic System 1984(4326)
-attr = str('&s_srs=102645&t_srs=4326')
-with open("formattedCoords.txt", "r") as file:
-	coords = file.read()
-file.close()
-remove('formattedCoords.txt')
+while it < len(lat):	
+	#Create file to store cordinates in proper format for REST API HTTP Request
+	filo = open('formattedCoords.txt', 'a+')
+	x = it
+	while ((x < it+200) and (x < len(lat))):
+		filo.write(str(lat[x])+','+str(lon[x])+';')
+		x+=1
+	filo.close()
+	it = x
+	print it
 
-#Making HTTP Request to the API
-url = http + coords + attr
-#Removes extra semicolon at the end of the coords
-url = url.replace(';&s_srs', '&s_srs')
-page = requests.get(url)
-content = page.content
-print "Response Code: " + str(page.status_code)
+	#API URL
+	http = str('http://epsg.io/trans?data=')
+	#Codes for conversion from US Feet Coordinates(102645) to World Geodetic System 1984(4326)
+	attr = str('&s_srs=102645&t_srs=4326')
+	with open("formattedCoords.txt", "r") as file:
+		coords = file.read()
+	file.close()
+	remove('formattedCoords.txt')
 
-#Grab the HTTP response and format to JSON
-resData = json.loads(content)
-lats = list()
-lons = list()
-for d in resData:
-	lats.append(d.get("y"))
-	lons.append(d.get("x"))
+	#Making HTTP Request to the API
+	url = http + coords + attr
+	#Removes extra semicolon at the end of the coords
+	url = url.replace(';&s_srs', '&s_srs')
+	page = requests.get(url)
+	content = page.content
+	print "Response Code: " + str(page.status_code)
+
+	#Grab the HTTP response and format to JSON
+	resData = json.loads(content)
+	lats = list()
+	lons = list()
+	for d in resData:
+		convertedLats.append(d.get("y"))
+		convertedLons.append(d.get("x"))
 
 #Create a dictionary from the converted lats and longs
-dic = {'lat':lats,'lon':lons}
+dic = {'lat':convertedLats,'lon':convertedLons}
 df = pd.DataFrame(dic)
 
 #Replace coordinates with new converted coordinates
@@ -58,6 +68,6 @@ data['Latitude'] = df['lat']
 data['Longitude'] = df['lon']
 
 #Specify which columns of the data to keep and save to csv file
-vital_columns = ["Ticket number","Issue Date","Issue time","Make","Body Style","Color","Location","Violation code","Violation Description","Fine amount","Latitude","Longitude"]
+vital_columns = ["Issue Date","Issue time","Make","Color","Location","Violation Description","Fine amount","Latitude","Longitude"]
 convertedFile = "translated_"+arg.filename
 data.to_csv(convertedFile,columns=vital_columns,header=vital_columns,index=False)
